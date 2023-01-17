b847a2a48eedd36224e16dc2edcbc203a9eeeffa
Andriy Kushnir (Orhideous) <me@orhideous.name>
2023-01-18 00:00:28 +0200

    Restructure network probe

--- probe.php
+++ probe.php
@@ -3,127 +3,134 @@
 Header("Cache-Control: no-cache, no-store, must-revalidate, max-age=0");
 mb_internal_encoding("UTF-8");
 
-include_once dirname(__FILE__) . '/php/common.php';
-$cfg = get_settings();
+$proxies = [
+    null,
+    ['gateway.keeps.cyou', 2081],
+    ['gateway.keepers.tech', 60789]
+];
 
+$forum = [
+    'rutracker.org',
+    'rutracker.net',
+    'rutracker.nl'
+];
+
+$api = [
+    'api.rutracker.cc',
+    'api.rutracker.net'
+];
+
+function getUrl($url, $proxy)
+{
+    try {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_HEADER => true,
+            CURLOPT_NOBODY => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS => 2,
+            CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+            CURLOPT_CONNECTTIMEOUT => 5,
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_URL => $url,
+        ]);
+        if (null !== $proxy) {
+            curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5_HOSTNAME);
+            curl_setopt($ch, CURLOPT_PROXY, sprintf("%s:%d", $proxy[0], $proxy[1]));
+        }
+        curl_exec($ch);
+        return curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    } catch (Exception $e) {
+        return null;
+    }
+}
+
+function checkAccess($proxies, $hostnames, $tpl)
+{
+    $res = [];
+    foreach ($hostnames as $hostname) {
+        $url = sprintf($tpl, $hostname);
+        foreach ($proxies as $proxy) {
+            $res[] = ['hostname'=> $hostname, 'proxy' => $proxy, 'code' => getUrl($url, $proxy)];
+        }
+    }
+    return $res;
+}
+
+$probe = new stdClass();
+
+$probe->configuration = new stdClass();
+$probe->connectivity = new stdClass();
+$probe->connectivity->forum = checkAccess($proxies, $forum, "https://%s/forum/info.php?show=copyright_holders");
+$probe->connectivity->api = checkAccess($proxies, $api, "https://%s/v1/get_client_ip");
+
+$probe->server = new stdClass();
+$probe->server->gateway = $_SERVER['GATEWAY_INTERFACE'];
+$probe->server->software = $_SERVER['SERVER_SOFTWARE'];
+
+$probe->php = new stdClass();
+$probe->php->version = phpversion();
+$probe->php->memory_limit = ini_get('memory_limit');
+$probe->php->max_execution_time = ini_get('max_execution_time');
+$probe->php->max_input_time = ini_get('max_input_time');
+$probe->php->max_input_vars = ini_get('max_input_vars');
+$probe->php->extensions = get_loaded_extensions();
 ?>
-
 <!DOCTYPE html>
-<html class="ui-widget-content">
-
+<html class="ui-widget-content" lang="en">
 <head>
-    <meta charset="utf-8" />
-    <title>web-TLO-<?php echo $webtlo->version ?></title>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/i18n/jquery-ui-i18n.min.js"></script>
-    <script src="scripts/jquery.mousewheel.min.js"></script>
-    <script src="scripts/js.cookie.min.js"></script>
-    <link rel="stylesheet" href="css/reset.css" /> <!-- сброс стилей -->
-    <link rel="stylesheet" href="css/style.css" /> <!-- таблица стилей webtlo -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <meta charset="utf-8"/>
+    <title>webTLO configuration checker</title>
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <script>
+        const binUrl = "https://bin.keepers.tech/upload";
+		async function sendData(url, data) {
+			const formData  = new FormData();
+			for(const name in data) {
+				formData.append(name, data[name]);
+			}
+			return await fetch(url, {method: 'POST', body: formData});
+		}
+
+		async function upload() {
+			let results = document.getElementById("result").textContent;
+			let payload = {"expiration": "1week", "syntax-highlight": "json", "content": results};
+			let resp = await sendData(binUrl, payload);
+
+			const p1 = document.createElement("p");
+            const p1text = document.createTextNode("Configuration shared");
+			p1.appendChild(p1text);
+			const p2 = document.createElement("p");
+			const p2url = document.createElement("a");
+			p2url.attributes['href'] = resp.url;
+			const p2text = document.createTextNode(resp.url);
+			p2url.appendChild(p2text);
+            p2.appendChild(p2url);
+            document.getElementById("shares").appendChild(p1);
+            document.getElementById("shares").appendChild(p2);
+        }
+		window.addEventListener("load", () => {
+			document.getElementById("send").addEventListener("click", upload);
+        })
+    </script>
 </head>
-
 <body>
-<?php
-	$probe = new stdClass();
-	
-	$probe->configuration = new stdClass();
-	$probe->configuration->version = $webtlo->version;
-	
-	$probe->connectivity = new stdClass();
-	$probe->connectivity->forum_url = $cfg['forum_url'];
-	$probe->connectivity->forum_ssl = $cfg['forum_ssl'];
-	$probe->connectivity->api_url = $cfg['api_url'];
-	$probe->connectivity->api_ssl = $cfg['api_ssl'];
-		
-	$probe->connectivity->forum_avaliability = new stdClass();
-	$probe->connectivity->forum_avaliability->from_server = "<span id='forum.server'>waiting for responce</span>";
-	$probe->connectivity->forum_avaliability->no_proxy = "<span id='forum.no'>waiting for responce</span>";
-	$probe->connectivity->forum_avaliability->keepers_tech = "<span id='forum.gateway.keepers.tech'>waiting for responce</span>";
-	$probe->connectivity->forum_avaliability->keeps_cyou = "<span id='forum.gateway.keeps.cyou'>waiting for responce</span>";
-	
-	$probe->connectivity->api_avaliability = new stdClass();
-	$probe->connectivity->api_avaliability->from_server = "<span id='api.server'>waiting for responce</span>";
-	$probe->connectivity->api_avaliability->no_proxy = "<span id='api.no'>waiting for responce</span>";
-	$probe->connectivity->api_avaliability->keepers_tech = "<span id='api.gateway.keepers.tech'>waiting for responce</span>";
-	$probe->connectivity->api_avaliability->keeps_cyou = "<span id='api.gateway.keeps.cyou'>waiting for responce</span>";
-
-	$probe->server = new stdClass();
-	$probe->server->gateway = $_SERVER['GATEWAY_INTERFACE'];
-	$probe->server->software = $_SERVER['SERVER_SOFTWARE'];
-	
-	$probe->php = new stdClass();
-	$probe->php->version = phpversion();
-	$probe->php->memory_limit = ini_get('memory_limit');
-	$probe->php->max_execution_time = ini_get('max_execution_time');
-	$probe->php->max_input_time = ini_get('max_input_time ');
-	$probe->php->max_input_vars = ini_get('max_input_vars ');
-	$probe->php->extensions = get_loaded_extensions();
-	
-
-	echo "<pre style='font-size: 14px; font-family: monospace;'>".json_encode($probe, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)."</pre>";
-
-?>
-
-    <script type="text/javascript">
-        var proxy_config = [
-            "proxy_type=socks5h&proxy_hostname=gateway.keeps.cyou&proxy_port=2081&proxy_activate_",
-            "proxy_type=socks5h&proxy_hostname=gateway.keepers.tech&proxy_port=60789&proxy_activate_"
-        ];
-		
-		async function test_connectivity_per_url(url, url_type) {
-			await test_connectivity_per_config(url, url_type, "");
-			await test_connectivity_per_config(url, url_type, proxy_config[0]+url_type);
-			await test_connectivity_per_config(url, url_type, proxy_config[1]+url_type);
-		}
-		async function test_connectivity_per_config(url, url_type, cfg) {
-			var proxy_hostname = cfg.split('&')?.find(i => i.includes("proxy_hostname"))?.split('=')[1] ?? "no";
-
-			$.ajax({
-				type: "POST",
-				url: "php/actions/check_mirror_access.php",
-				data: {
-					cfg: cfg,
-					url: url,
-					url_custom: null,
-					ssl: true,
-					url_type: url_type
-				},
-				success: function (response) {
-					var id = url_type+"."+proxy_hostname;
-					document.getElementById(id).innerHTML = response ? true : false;
-				}
-			});
-		}
-
-		function get_endpoint(url, callback) {
-			$.ajax({
-				type: "POST",
-				url: "php/actions/get_endpoint.php",
-				data: {
-					url: url
-				},
-				success: function (response) {
-					callback(response);
-				}
-			});
-		}
-		function get_connectivity_from_server(name, url){
-			get_endpoint(url, response => {
-				var object = JSON.parse(response);
-				var status = object.results[object.results.length - 1].conditionResults[0].success;
-				document.getElementById(name).innerHTML = status;
-			});
-			
-		}
-		
-		get_connectivity_from_server("forum.server", "https://status.keepers.tech/api/v1/endpoints/forum_main/statuses");
-		get_connectivity_from_server("api.server", "https://status.keepers.tech/api/v1/endpoints/api_main/statuses");
-		
-		test_connectivity_per_url("<?php echo $cfg['forum_url']; ?>", "forum");
-		test_connectivity_per_url("<?php echo $cfg['api_url']; ?>" , "api");
-	</script>
+<header>
+    <h1>webTLO configuration checker</h1>
+    <button id="send">Share my config</button>
+    <div id="shares"></div>
+</header>
+<main>
+    <h3>Configuration</h3>
+<pre id="result">
+<?php echo json_encode($probe, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES); ?>
+</pre>
+</main>
+<footer>
+    <p>made with ☕</p>
+</footer>
 </body>
-
 </html>
